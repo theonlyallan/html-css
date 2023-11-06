6a030930b1dc60be390f55479431fead5e9a2698
Allan Lombarde Ramos <allanlombardeallan@gmail.com>
2023-11-06 17:13:04 -0300

    Update cadastro.php

--- exércicios/ex025/cadastro.php
+++ exércicios/ex025/cadastro.php
@@ -1,1 +1,28 @@
-Os dados estão aqui!
+<?php
+if ($_SERVER["REQUEST_METHOD"] === "GET") {
+    if (isset($_GET["nome"])) {
+        $nome = $_GET["nome"];
+        echo "Nome: " . $nome . "<br>";
+    }
+
+    if (isset($_GET["media"])) {
+        $media = $_GET["media"];
+        echo "Média: " . $media . "<br>";
+    }
+
+    if (isset($_GET["mes"])) {
+        $mes = $_GET["mes"];
+        echo "Período Letivo: " . $mes . "<br>";
+    }
+
+    if (isset($_GET["dia"])) {
+        $dia = $_GET["dia"];
+        echo "Dia da Prova: " . $dia . "<br>";
+    }
+
+    if (isset($_GET["hora"])) {
+        $hora = $_GET["hora"];
+        echo "Horário da Prova: " . $hora . "<br>";
+    }
+}
+?>
